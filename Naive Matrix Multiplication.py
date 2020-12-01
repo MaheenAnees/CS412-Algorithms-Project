304c7a0c74821ec5e613af36717ab2d90238f1bc
Rabeea Atif Rana <54802882+rabeeaatif@users.noreply.github.com>
2020-12-01 20:55:53 +0500

    Added time complexity code

--- Naive Matrix Multiplication.py
+++ Naive Matrix Multiplication.py
@@ -35,3 +35,40 @@ def main():
         print(i)
         
 main()
+
+######################## C O M P L E X I T Y ########################################
+
+import timeit
+code_to_test = """
+def naive_matrix_mult(M1, M2, result):
+# iterate through rows of M1
+    for i in range(len(M1)):
+       # iterate through columns of M2
+       for j in range(len(M2[0])):
+           # iterate through rows of M2
+           for k in range(len(M2)):
+               result[i][j] += M1[i][k] * M2[k][j]
+    return result
+
+def main():
+    # 3x3 matrix
+    M1 = [[1,2,3,4],[5,6,7,8],[4,3,2,1],[8,7,6,5]] #4x4 matrix
+    
+    # 3x4 matrix
+    M2 = [[1,2,3,4],[5,6,7,8],[4,3,2,1],[8,7,6,5]] #4x4 matrix
+        
+    #Resulting matrix = 4 x 4
+    Res = [[0,0,0,0],
+             [0,0,0,0],
+             [0,0,0,0],[0,0,0,0]]
+        
+    result = naive_matrix_mult(M1, M2, Res)
+
+main()
+
+
+
+"""
+elapsed_time = timeit.timeit(code_to_test, number=100)/100
+print("TIME FOR NAIVE ALGORITHM: ", elapsed_time)
+
